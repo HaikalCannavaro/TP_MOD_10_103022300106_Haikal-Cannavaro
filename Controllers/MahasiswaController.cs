8df2a62af648b2bb648a970845e77f88cf0dc6ed
HaikalCannavaro <haikalcannavaro64@gmail.com>
2025-04-27 19:17:45 +0700

    menambahkan API mahasiswa

---
+++ Controllers/MahasiswaController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using tpmodul10_103022300106.Models;
+
+namespace tpmodul10_103022300106.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> mahasiswaList = new List<Mahasiswa>
+        {
+            new Mahasiswa { Nama = "LeBron James", Nim = "1302000001" },
+            new Mahasiswa { Nama = "Stephen Curry", Nim = "1302000002" },
+            new Mahasiswa { Nama = "Kevin Durant", Nim = "1302000003" }
+        };
+
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return mahasiswaList;
+        }
+
+        [HttpGet("{index}")]
+        public ActionResult<Mahasiswa> Get(int index)
+        {
+            if (index < 0 || index >= mahasiswaList.Count)
+                return NotFound();
+            return mahasiswaList[index];
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Mahasiswa mahasiswa)
+        {
+            mahasiswaList.Add(mahasiswa);
+            return CreatedAtAction(nameof(Get), new { index = mahasiswaList.Count - 1 }, mahasiswa);
+        }
+
+        [HttpDelete("{index}")]
+        public IActionResult Delete(int index)
+        {
+            if (index < 0 || index >= mahasiswaList.Count)
+                return NotFound();
+            mahasiswaList.RemoveAt(index);
+            return NoContent();
+        }
+    }
+}
